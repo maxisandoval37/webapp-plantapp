41a6ed11636f2f448af490ebb3daa1b49758e636
Maxi Sandoval <maxisandoval98@gmail.com>
2024-09-17 22:12:49 -0300

    se agregan mas datos de prueba sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,9 +13,19 @@ INSERT INTO prospecto (nombre, cantidad, unidad) VALUES ('Fertilizante', 15.0, '
 INSERT INTO prospecto (nombre, cantidad, unidad) VALUES ('Agua destilada', 100.0, 'L');
 INSERT INTO prospecto (nombre, cantidad, unidad) VALUES ('Humus de lombriz', 2.0, 'KG');
 INSERT INTO prospecto (nombre, cantidad, unidad) VALUES ('Arena', 50.0, 'KG');
+INSERT INTO Prospecto (nombre, cantidad, unidad) VALUES ('Maceta', 2.0, 'Cant');
 
 -- Inserción de Plantas de ejemplo:
 insert into planta (especie, color_hojas, fecha_plantacion, jardinero_id) VALUES ('Venus atrapamoscas', 'Verdes', '2023-01-10', 1);
+INSERT INTO planta (especie, color_hojas, fecha_plantacion, jardinero_id) VALUES ('Suculenta', 'Rojas', '2023-01-10', 1);
+INSERT INTO planta (especie, color_hojas, fecha_plantacion, jardinero_id) VALUES ('Tomate', 'Verdes', '2023-01-10', 3);
+INSERT INTO planta (especie, color_hojas, fecha_plantacion, jardinero_id) VALUES ('Lengua de suegra', 'Verdes', '2023-01-10', 3);
+INSERT INTO planta (especie, color_hojas, fecha_plantacion, jardinero_id) VALUES ('Flores', 'Violetas', '2023-01-10', 1);
+INSERT INTO planta (especie, color_hojas, fecha_plantacion, jardinero_id) VALUES ('Lechuga', 'Verdes', '2023-01-10', 1);
+INSERT INTO planta (especie, color_hojas, fecha_plantacion, jardinero_id) VALUES ('Rosas', 'Rosa', '2023-01-10', 4);
 
 -- Inserción de Plantas de ejemplo con prospectos:
-INSERT INTO planta_prospecto (planta_id, prospecto_id) VALUES (1,1);
+INSERT INTO planta_prospecto (planta_id, prospecto_id) VALUES (3,2);
+INSERT INTO planta_prospecto (planta_id, prospecto_id) VALUES (1,3);
+INSERT INTO planta_prospecto (planta_id, prospecto_id) VALUES (2,2);
+INSERT INTO planta_prospecto (planta_id, prospecto_id) VALUES (2,4);
