68a991e9135232ec5334677e9c2c2b447400ae99
Maxi Sandoval <maxisandoval98@gmail.com>
2024-10-10 18:12:18 -0300

    import logger

--- src/main/java/ar/dev/maxisandoval/webappplantapp/controller/AuthController.java
+++ src/main/java/ar/dev/maxisandoval/webappplantapp/controller/AuthController.java
@@ -3,6 +3,7 @@ package ar.dev.maxisandoval.webappplantapp.controller;
 import ar.dev.maxisandoval.webappplantapp.model.Usuario;
 import ar.dev.maxisandoval.webappplantapp.service.CustomUserDetailsService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
